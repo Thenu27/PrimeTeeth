515922e736fa2cc409016ed33b43bda0dbf14e39
Thenuka Liyanage <thenuliyana@gmail.com>
2026-07-19 16:35:54 +0530

    Testing

--- src/components/ContactBar/ContactBar.jsx
+++ src/components/ContactBar/ContactBar.jsx
@@ -32,7 +32,7 @@ const ContactBar = () => {
       <div className="contact-category">
         <img src="/smartphone.svg" alt="Phone" />
         <p className="contact-category-title">Contact</p>
-        <a href={`tel:${contactBar[0].contact}`} className="contact-category-info">
+        <a href={`tel:(+94) 77 749 0691`} className="contact-category-info">
           (+94) 77 749 0691
         </a>     
       </div>
